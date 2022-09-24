b47d0ccd1188f5092945179a5ed274cb9943e033
Bedo <bedoabdenaser@gmail.com>
2022-09-24 21:57:30 +0200

    adding readme file

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
+# Project Title
 # Landing Page Project
 
-## Table of Contents
+# Table of Contents
 
-* [Instructions](#instructions)
+* [Project Title](#navbar__menu)
+* [Landing Page Project](#main__hero)
+* [Table of Contents](#section1)
+* [Project Description](#section2)
+* [Contribute](#section3)
+    * [Adding new features or fixing bugs](#section4)
 
-## Instructions
+# Project Description
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+> this page builds with HTML5, CSS3, and JavaScript to create the page with a dynamic navigation menu depending on the number of sections on the page, and it's highlighting the section and the section nav link depending on the section displaying on the page, also the page has scroll to top button.
 
-To get started, open `js/app.js` and start building out the app's functionality
+# Contribute 
+  
+### Adding new features or fixing bugs 
+  
+[(Back to top)](#table-of-contents) 
+  
+    fell free to add new features or edit the page by cloning the repo.
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
--- js/app.js
+++ js/app.js
@@ -18,49 +18,47 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
-*/
+// define global values
 const unOrderedList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 const fragment = document.createDocumentFragment();
 const toTop = document.querySelector('.scroll');
-    const links = document.querySelectorAll('.menu__link');
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+const links = document.querySelectorAll('.menu__link');
+const navIcon = document.querySelector('.nav-icon');
+// 
+
+// start build nav menu
 function navMenu (){
     sections.forEach(function (section){
         const secHeader = section.dataset.nav;
         const item = document.createElement('li');
         const itemSpan = document.createElement('span');
         itemSpan.textContent = secHeader;
-        itemSpan.setAttribute('data-nav',section.id)
+        itemSpan.setAttribute('data-nav',section.id);
         itemSpan.classList.add('menu__link');
+        // function to scroll to the clicked section
         itemSpan.addEventListener('click', ()=>{
             section.scrollIntoView({
-                behavior:"smooth",
+                behavior:'smooth',
             });
-            showMenu()
+            // call function to hide the sections links when it's clicked
+            showMenu();
         });
         item.appendChild(itemSpan);
         fragment.appendChild(item);
     });
     unOrderedList.appendChild(fragment);
 }
+// event to hold building the nav till the the page is loaded
 window.addEventListener('load', navMenu);
+// end build nav menu
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
+// on scroll, active the shown section
 window.addEventListener('scroll', function(){
+    // loop to loop over the sections
     for (const section of sections) {
-        let activeLink = document.querySelector(`span[data-nav="${section.id}"]`)
+        let activeLink = document.querySelector(`span[data-nav="${section.id}"]`);
+        // condition to know which section id shown
         if(window.scrollY >= section.offsetTop - section.offsetHeight*0.25 && window.scrollY <= section.offsetTop + section.offsetHeight*0.75){
             section.classList.add('your-active-class');
             links.forEach((activeLink)=>{
@@ -73,40 +71,30 @@ window.addEventListener('scroll', function(){
         }else{
             section.classList.remove('your-active-class');
             activeLink.classList.remove('selected');
-        }
-    }
+        };
+    };
+    // condition to detect when to add the scroll to top button
     if(window.scrollY >= 600){
-        toTop.classList.add('active')
+        toTop.classList.add('active');
     }else{
-        toTop.classList.remove('active')
-    }
+        toTop.classList.remove('active');
+    };
 });
-toTop.addEventListener('click', ()=> window.scrollTo({top:0,behavior:"smooth"}))
-// build the nav    
-const navIcon = document.querySelector('.nav-icon')
+// 
+
+// add scroll to the top button when u swipe down
+toTop.addEventListener('click', ()=> window.scrollTo({top:0,behavior:"smooth"}));
+// 
+
+// show the menu on click
 navIcon.addEventListener('click',()=>{
-    showMenu()
-})
+    showMenu();
+});
+// function to toggle between remove or add the class when it's called
 function showMenu(){
-    navIcon.classList.toggle('x-mark')
-    unOrderedList.classList.toggle('show-menu')
-}
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+    navIcon.classList.toggle('x-mark');
+    unOrderedList.classList.toggle('show-menu');
+};
+// 
 
 
